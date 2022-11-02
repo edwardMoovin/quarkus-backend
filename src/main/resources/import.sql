16e5ac55491d7b19d088bb0ba07cf72719771cb7
edwardMoovin <ejimenez@moovin.me>
2022-11-01 23:31:17 -0600

    Crear el usuario de pruebas

---
+++ src/main/resources/import.sql
@@ -0,0 +1,5 @@
+INSERT INTO
+    userclient(id, username, password)
+VALUES
+    (1, 'test',
+     'pQ1TF8GjGt57sasCV9ddsTJXtL4guVupHIaXXwiwImaLpR96q9Yl+muaj106AleCcb/mwby+wpLWUJzPziUnQC+eTUpXEAcei6/k9qZL0+mmKKMmHncPyR9gznMZtF4BxCmGUGcbfPycEcYffuh9KPIDk5UturZVsE1WhSm8mDkTzuSeFYY0iYd7kJBHbfLKf3kPN96GbfYTaUT+d/bmH05phlAFF4Bnm3haKy79k/EQbB4ltkAaby2ZjseQWbSTQZxwyaAvNYEpeNe6U0nuOSjRmPN6KjI5pUDwRP5Q9YmBAC5y4XG1HKTMfZFmCnBbz9v81V6/bOwWV7ivaoKedw==');
